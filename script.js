dd570798294c3b0b521585df7fca4099a4e77e11
nolythedev <nolychigwedere@hotmail.co.uk>
2024-01-08 00:43:22 +0000

    Generate password function added

--- script.js
+++ script.js
@@ -16,14 +16,16 @@ var pwUppercase = false;
 var pwNumeric = false;
 var pwSpecialChar = false;
 
+ // Prompt user to enter number of desired password characters
+ var pwLength = prompt("How many characters would you like to include in your Password?");
+
+ //Convert prompt string to number
+ var pwLengthNum = parseFloat(pwLength);
+
+ pwLength = pwLengthNum;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
-  // Prompt user to enter number of desired password characters
-  var pwLength = prompt("How many characters would you like to include in your Password?");
-
-  //Convert prompt string to number
-  var pwLengthNum = parseFloat(pwLength);
 
   // Check user's input is between 8 and 128 chars
   if (pwLengthNum >= 8 && pwLengthNum <= 128) {
@@ -31,21 +33,17 @@ function getPasswordOptions() {
 
     // Confirm user would like to include lowercase chars
     var checkLowercase = confirm("Would you like to include lowercase characters?");
-
-    // Set password lowercase to true if user selects true
-    checkLowercase ? pwLowercase = checkLowercase : console.log("You did not choose any lower characters");
+    pwLowercase = checkLowercase;
 
     // Confirm user would like to include uppercase chars
     var checkUppercase = confirm("Would you like to include uppercase characters?");
-
-    // Set password uppercase to true if user selects true
-    checkUppercase ? pwUppercase = checkUppercase : console.log("You did not choose any uppercase characters");
+    pwUppercase = checkUppercase;
 
     var checkNumeric = confirm("Would you like to include numeric characters?");
-    checkNumeric ? pwNumeric = checkNumeric : console.log("You did not choose any numeric characters");
+    pwNumeric = checkNumeric;
 
     var checkSpecialChar = confirm("Would you like to include special characters?");
-    checkSpecialChar ? pwSpecialChar = checkSpecialChar : console.log("You did not choose any special characters");
+   pwSpecialChar = checkSpecialChar;
 
   } else {
     console.log("Your password should be between 8 - 128 characters long to continue");
@@ -57,24 +55,39 @@ getPasswordOptions();
 // Function for getting a random element from an array
 function getRandom(arr) {
 
-  // Get random index value
+  // Get random index value of array being passed through 
   var randomIndex = Math.floor(Math.random() * arr.length);
 
-  // Get random character 
+  // Get random character
   var randomChar = arr[randomIndex];
   
   return randomChar;
 }
 
-console.log(getRandom(specialCharacters));
-console.log(getRandom(numericCharacters));
-console.log(getRandom(lowerCasedCharacters));
-console.log(getRandom(upperCasedCharacters));
+console.log(pwLength);
+
+var userPassword = "";
 
 // Function to generate password with user input
 function generatePassword() {
 
+  while (userPassword.length < pwLength) {
+    pwLowercase ? userPassword += getRandom(lowerCasedCharacters) : console.log("You did not choose any lower characters");
+
+    pwUppercase ? userPassword += getRandom(upperCasedCharacters) : console.log("You did not choose any uppercase characters");
+  
+    pwNumeric ? userPassword += getRandom(numericCharacters) : console.log("You did not choose any numeric characters");
+
+    pwSpecialChar ? userPassword += getRandom(specialCharacters) : console.log("You did not choose any special characters");
+   
+    if (userPassword.length >= pwLength) {
+      break;
+    }
+  }
+  return userPassword;
 }
+
+generatePassword(userPassword);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
